19f5deb1c407241a8ef3499814834cff0c645903
Surbhi Mayank <58289829+surbhi2408@users.noreply.github.com>
2020-12-09 22:33:14 +0530

    chat screen updated

--- lib/screens/chatting_screen.dart
+++ lib/screens/chatting_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:chat_app/widgets/full_image_widget.dart';
 import 'package:chat_app/widgets/progress_widget.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
@@ -681,10 +682,118 @@ class ChatScreenState extends State<ChatScreen> {
     );
   }
 
-  Widget createItem(int index, DocumentSnapshot document){
-    
+  isLastMsgLeft(int index){
+    if((index > 0 && listMessage != null && listMessage[index - 1]["idFrom"] == id) || index == 0){
+      return true;
+    }
+    else{
+      return false;
+    }
   }
 
+  isLastMsgRight(int index){
+    if((index > 0 && listMessage != null && listMessage[index - 1]["idFrom"] != id) || index == 0){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  // function to display messages
+  Widget createItem(int index, DocumentSnapshot document){
+    // sender messages - right side
+    if(document["idFrom"] == id){
+      return Row(
+        children: <Widget>[
+          document["type"] == 0
+
+              // text msg
+              ? Container(
+                  child: Text(
+                    document["content"],
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
+                  width: 200.0,
+                  decoration: BoxDecoration(
+                    color: Colors.lightBlueAccent,
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  margin: EdgeInsets.only(bottom: isLastMsgRight(index) ? 20.0 : 10.0, right: 10.0),
+                )
+
+              // image msg
+              : document["type"] == 1
+              ? Container(
+                  child: FlatButton(
+                    child: Material(
+                      child: CachedNetworkImage(
+                        placeholder: (context, url) => Container(
+                          child: CircularProgressIndicator(
+                            valueColor: AlwaysStoppedAnimation<Color>(Colors.lightBlueAccent),
+                          ),
+                          width: 200.0,
+                          height: 200.0,
+                          padding: EdgeInsets.all(70.0),
+                          decoration: BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                          ),
+                        ),
+                        errorWidget: (context, url, error) => Material(
+                          child: Image.asset(
+                              "assets/images/img_not_available.jpeg",
+                            width: 200.0,
+                            height: 200.0,
+                            fit: BoxFit.cover,
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                          clipBehavior: Clip.hardEdge,
+                        ),
+                        imageUrl: document["content"],
+                        width: 200.0,
+                        height: 200.0,
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                      clipBehavior: Clip.hardEdge,
+                    ),
+                    onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(
+                          builder: (context) => FullPhoto(url: document["content"])
+                      ));
+                    },
+                  ),
+                  margin: EdgeInsets.only(bottom: isLastMsgRight(index) ? 20.0 : 10.0, right: 10.0),
+                )
+
+              // sticker .gif msg
+              : Container(
+                  child: Image.asset(
+                    "assets/stickers/${document['content']}.gif",
+                    width: 100.0,
+                    height: 100.0,
+                    fit: BoxFit.cover,
+                  ),
+                  margin: EdgeInsets.only(bottom: isLastMsgRight(index) ? 20.0 : 10.0, right: 10.0),
+                ),
+        ],
+        mainAxisAlignment: MainAxisAlignment.end,
+      );
+    }
+    // receiver messages - left side
+    else{
+      return Container(
+        
+      );
+    }
+  }
+
+  // function to create text Field where user can write messages
   createInput(){
     return Container(
       child: Row(
---
+++ lib/widgets/full_image_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class FullPhoto extends StatelessWidget {
+
+  final String url;
+
+  FullPhoto({Key key, @required this.url}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class FullPhotoScreen extends StatefulWidget {
+  @override
+  State createState() => FullPhotoScreenState();
+}
+
+class FullPhotoScreenState extends State<FullPhotoScreen> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
